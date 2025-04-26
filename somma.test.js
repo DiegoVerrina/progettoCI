d1d1e20037df37083eec97a8fd302f647861c5dd
diego <verrinadieo@gmail.com>
2025-04-26 15:32:17 +0200

    first commit

---
+++ somma.test.js
@@ -0,0 +1,25 @@
+const { add } = require('./somma');
+const { sub } = require('./somma');
+
+test('add(2, 3) should return 5', () => {
+  expect(add(2, 3)).toBe(5);
+});
+
+test('add(-1, 1) should return 0', () => {
+  expect(add(-1, 1)).toBe(0);
+});
+
+test('add(0, 0) should return 0', () => {
+  expect(add(0, 0)).toBe(0);
+});
+
+test('sub(5, 3) should return 2', () => {
+  expect(sub(5, 3)).toBe(2);
+}
+);
+test('sub(3, 5) should return -2', () => {
+  expect(sub(3, 5)).toBe(-2);
+});
+test('sub(0, 0) should return 0', () => {
+  expect(sub(0, 0)).toBe(0);
+});
